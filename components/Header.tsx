d2ca368652b4339b150a014d08d4bba672f331f4
Co9xs <ryo.fjsm0203@icloud.com>
2021-12-02 10:00:48 +0900

    Next Authの導入と設定

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/client";
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
+
+  const [session, loading] = useSession();
 
   let left = (
     <div className="left">
